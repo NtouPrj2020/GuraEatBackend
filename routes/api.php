3229c3b347985395c46a8c5bb9a4cc698e7d2af8
wc2014920 <wc2014920@gmail.com>
2020-11-29 19:56:10 +0800

    restore commit

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,6 @@ Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'users', 'middleware' => ['auth:sanctum']], function () {
         Route::group(['prefix' => 'customer'], function () {
             Route::post('/logout', 'App\Http\Controllers\AuthController@customerLogout');
-            Route::get('/info', 'App\Http\Controllers\CustomerController@customerAccount');
         });
         Route::group(['prefix' => 'delivery_man'], function () {
             Route::post('/logout', 'App\Http\Controllers\AuthController@deliveryManLogout');
