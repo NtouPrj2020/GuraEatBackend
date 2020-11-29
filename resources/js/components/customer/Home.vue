864af4290504ce1064daa4219f5ca4b1fa3dbc9b
azaz0846 <azaz0846@gmail.com>
2020-11-29 17:43:01 +0800

    feat: home.vue

--- resources/js/components/customer/Home.vue
+++ resources/js/components/customer/Home.vue
@@ -3,56 +3,65 @@
         <div class="panel-heading text-center">Home</div>
         <div class="container">
             <v-main>
-                <v-card
-                    :loading="loading"
-                    class="mx-auto my-12"
-                    max-width="374"
-                >
-                    <template slot="progress">
-                        <v-progress-linear
-                            color="deep-purple"
-                            height="10"
-                            indeterminate
-                        ></v-progress-linear>
-                    </template>
-
+                <v-card class="mx-auto" max-width="400">
                     <v-img
-                        height="250"
-                        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                    ></v-img>
+                        class="white--text align-end"
+                        height="200px"
+                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    >
+                        <v-card-title>Top 10 Australian beaches</v-card-title>
+                    </v-img>
 
-                    <v-card-title>Cafe Badilico</v-card-title>
+                    <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
 
-                    <v-card-text>
-                        <v-row
-                            align="center"
-                            class="mx-0"
-                        >
-                            <v-rating
-                                :value="4.5"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                            ></v-rating>
-                            W
-                            <div class="grey--text ml-4">
-                                4.5 (413)
-                            </div>
-                        </v-row>
+                    <v-card-text class="text--primary">
+                        <div>Whitehaven Beach</div>
 
-                        <div class="my-4 subtitle-1">
-                            $ • Italian, Cafe
-                        </div>
-
-                        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio
-                            seating.
-                        </div>
+                        <div>Whitsunday Island, Whitsunday Islands</div>
                     </v-card-text>
 
+                    <v-card-actions>
+                        <v-btn color="orange" text> Share </v-btn>
+
+                        <v-btn color="orange" text> Explore </v-btn>
+                    </v-card-actions>
+                </v-card>
+                <v-card class="mx-auto" max-width="400">
+                    <v-img
+                        class="white--text align-end"
+                        height="200px"
+                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    >
+                        <v-card-title>Top 10 Australian beaches</v-card-title>
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+                    <v-card-text class="text--primary">
+                        <div>Whitehaven Beach</div>
+
+                        <div>Whitsunday Island, Whitsunday Islands</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn color="orange" text> Share </v-btn>
+
+                        <v-btn color="orange" text> Explore </v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-main>
         </div>
     </div>
 </template>
+<script>
+import getRestaurantall from "../../api";
+export default {
+    data: () => ({
+        getRestaurantall(
+            {"page"}
+        ).then((res) => { console.table(res.data) })
+    .catch((error) => { console.error(error) })
+    }),
+    mount() {},
+};
+</script>
