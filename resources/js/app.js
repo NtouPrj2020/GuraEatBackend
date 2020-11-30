ec109c089f14ee923f6a855aa8cb90b49034f88a
skyblue <bob890823@gmail.com>
2020-11-30 18:06:44 +0800

    feat: 登入頁面的密碼錯誤訊息,新增showsnackbar()
    輸入錯誤雌料會顯示錯誤訊息
    登入成功會存入mode,user_name,token
    可以直接使用showsnackbar()

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,42 +1,48 @@
-require('./bootstrap');
-import Vue from 'vue'
-import Vuetify from 'vuetify';
-import Vuex from 'vuex';
-import VueRouter from 'vue-router'
+require("./bootstrap");
+import Vue from "vue";
+import Vuetify from "vuetify";
+import Vuex from "vuex";
+import VueRouter from "vue-router";
 
-import router from './routes';
-import App from './components/App.vue';
-import 'vuetify/dist/vuetify.min.css'
+import router from "./routes";
+import App from "./components/App.vue";
+import "vuetify/dist/vuetify.min.css";
 import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 Vue.use(Vuetify);
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 const vuetify = new Vuetify({
     theme: {
         themes: {
             light: {},
-            dark: {},
-        },
+            dark: {}
+        }
     },
     icons: {
-        iconfont: 'fa4', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
-    },
-})
+        iconfont: "fa4" // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    }
+});
 
 const store = new Vuex.Store({
     plugins: [createPersistedState()],
     state: {
         user_email: "",
+        user_name: "",
         mode: 2, //0admin 1user 2guest
         access_token: null,
         device_name: "",
-        cstfToken: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        cstfToken: document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute("content")
     },
     actions: {},
     mutations: {
         USER_EMAIL(state, user_email) {
             state.user_email = user_email;
+        },
+        USER_NAME(state, user_name) {
+            state.user_name = user_name;
         },
         MODE(state, mode) {
             state.mode = mode;
@@ -46,11 +52,14 @@ const store = new Vuex.Store({
         },
         DEVICE_NAME(state, device_name) {
             state.device_name = device_name;
-        },
+        }
     },
     getters: {
         getMode: state => {
             return state.mode;
+        },
+        getUserName: state => {
+            return state.user_name;
         },
         getAccessToken: state => {
             return state.access_token;
@@ -65,17 +74,16 @@ const store = new Vuex.Store({
             return state.cstfToken;
         }
     }
-})
+});
 
+router.mode = "html5";
 
-router.mode = 'html5';
-
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     store: store,
     vuetify: vuetify,
     router: router,
-    render: h => h(App),
+    render: h => h(App)
 });
--- resources/js/components/customer/CustomerApp.vue
+++ resources/js/components/customer/CustomerApp.vue
@@ -1,56 +1,59 @@
 <template>
-    <div class="container">
-            <router-view name="customerView"></router-view>
+  <div class="container">
+    <router-view
+      name="customerView"
+      v-on:showSnackBar="showSnackBar"
+    ></router-view>
 
-            <v-bottom-navigation fixed v-model="value">
-                <v-btn value="home" @click="toHome()">
-                    <span>首頁</span>
+    <v-bottom-navigation fixed v-model="value">
+      <v-btn value="home" @click="toHome()">
+        <span>首頁</span>
 
-                    <v-icon>mdi-home</v-icon>
-                </v-btn>
-                <v-btn value="search" @click="toSearch()">
-                    <span>搜尋</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn value="search" @click="toSearch()">
+        <span>搜尋</span>
 
-                    <v-icon>mdi-magnify</v-icon>
-                </v-btn>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
 
-                <v-btn value="cart" @click="toCart()">
-                    <span>購物車</span>
+      <v-btn value="cart" @click="toCart()">
+        <span>購物車</span>
 
-                    <v-icon>mdi-cart</v-icon>
-                </v-btn>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
 
-                <v-btn value="info" @click="toInfo()">
-                    <span>資訊</span>
+      <v-btn value="info" @click="toInfo()">
+        <span>資訊</span>
 
-                    <v-icon>mdi-account-circle-outline</v-icon>
-                </v-btn>
-            </v-bottom-navigation>
-    </div>
+        <v-icon>mdi-account-circle-outline</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-    },
-    mounted() {
-
-    },
-    data: () => ({
-    }),
-    methods: {
-        toHome() {
-            this.$router.push("/customer/home");
-        },
-        toSearch() {
-            this.$router.push("/customer/search");
-        },
-        toCart() {
-            this.$router.push("/customer/cart");
-        },
-        toInfo() {
-            this.$router.push("/customer/info");
-        },
+  props: {},
+  mounted() {
+    if (this.$store.getters.getMode == 0) {
+      this.$router.push("login");
     }
+  },
+  data: () => ({}),
+  methods: {
+    toHome() {
+      this.$router.push("/customer/home");
+    },
+    toSearch() {
+      this.$router.push("/customer/search");
+    },
+    toCart() {
+      this.$router.push("/customer/cart");
+    },
+    toInfo() {
+      this.$router.push("/customer/info");
+    },
+  },
 };
 </script>
