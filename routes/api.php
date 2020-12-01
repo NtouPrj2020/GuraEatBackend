f91121170cfd202ab72b20b6554ac76c2c278236
Flexolk <aci5722@gmail.com>
2020-12-01 17:10:50 +0800

    feat: reset

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,9 @@ Route::group(['prefix' => 'v1'], function () {
             Route::post('/logout', 'App\Http\Controllers\AuthController@customerLogout');
             Route::get('/restaurant/all', 'App\Http\Controllers\CustomerGetRestaurantInfoController@restaurants');
             Route::get('/info', 'App\Http\Controllers\CustomerInfoController@getCustomer');
+            Route::get('/restaurant/searchByID', 'App\Http\Controllers\CustomerInfoController@getRestaurantByID');
+            Route::get('/restaurant/searchByKeyword', 'App\Http\Controllers\CustomerInfoController@getRestaurantByKeyword');
+            Route::get('/restaurant/searchByTag', 'App\Http\Controllers\CustomerInfoController@getRestaurantByTag');
         });
         Route::group(['prefix' => 'delivery_man'], function () {
             Route::post('/logout', 'App\Http\Controllers\AuthController@deliveryManLogout');
