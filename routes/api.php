486a36c89cb1e967aacaa486732bdcb4dbddd14a
wc2014920 <wc2014920@gmail.com>
2020-12-08 17:11:04 +0800

    feat:GeographyController 修正editDeliveryManLocation().新增輸出:經緯度 修正getDeliveryManLocation()

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,8 @@ Route::group(['prefix' => 'v1'], function () {
             Route::put('/info', 'App\Http\Controllers\DeliveryManInfoController@editDeliveryMan');
             Route::put('/status', 'App\Http\Controllers\DeliveryManChangeStateController@changeState');
             Route::post('/logout', 'App\Http\Controllers\DeliveryManInfoController@deliveryManLogout');
+            Route::get('/location', 'App\Http\Controllers\GeographyController@getDeliveryManLocation');
+            Route::post('/location', 'App\Http\Controllers\GeographyController@editDeliveryManLocation');
         });
         Route::group(['prefix' => 'restaurant'], function () {
             Route::get('/info', 'App\Http\Controllers\RestaurantInfoController@getRestaurant');
