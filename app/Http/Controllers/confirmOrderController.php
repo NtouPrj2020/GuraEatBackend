d7b92b38b00e09fb116ab965b64231fb85225728
wc2014920 <wc2014920@gmail.com>
2020-12-13 10:30:57 +0800

    feat:confirmOrderController 修正

--- app/Http/Controllers/confirmOrderController.php
+++ app/Http/Controllers/confirmOrderController.php
@@ -7,84 +7,93 @@ use App\Models\RestaurantRate;
 use App\Models\DeliveryManRate;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
 class confirmOrderController extends Controller
 {
-    //
-    public function xx($request)
+    public function addStar($Rate,$star)
     {
         # code...
-        
+        switch ($star) {
+            case '1':
+                # code...
+                $Rate->star1 +=1;
+                break;
+            case '2':
+                # code...
+                $Rate->star2 +=1;
+                break;
+            case '3':
+                # code...
+                $Rate->star3 +=1;
+                break;
+            case '4':
+                # code...
+                $Rate->star4 +=1;
+                break;
+            case '5':
+                # code...
+                $Rate->star5 +=1;
+                break;
+            default:
+                # code...
+                break;
+        }
+        $Rate->save();
+        return $Rate;
     }
     public function rateOrder(Request $request)
     {
         $customer = $request->user();
-        $request->validate([//input
+        $request->validate([
             'delivery_man_star' => 'required|int',
             'restaurant_star' => 'required|int',
         ]);
-        $finishedOrder = $customer->orders()->where('status','=','2')->where('customer_id','=',$customer->id)->first();
-        $chooseMan = $finishedOrder->delivery_man_id;$chooseRestaurant = $finishedOrder->restaurant_id;
-        $DeliveryManRate = DeliveryManRate::where('delivery_man_id','=',$chooseMan)->first();
-        $restaurantRate = RestaurantRate::where('restaurant_id','=',$chooseRestaurant)->first();
-        $DeliveryManRate = DeliveryManRate::findorfail($DeliveryManRate->id);
-        $restaurantRate = RestaurantRate::findorfail($restaurantRate->id);
-        // dd($restaurantRate);
-        switch ($request->delivery_man_star) {
-            case '1':
-                # code...
-                $DeliveryManRate->star1 +=1;
-                break;
-            case '2':
-                # code...
-                $DeliveryManRate->star2 +=1;
-                break;
-            case '3':
-                # code...
-                $DeliveryManRate->star3 +=1;
-                break;
-            case '4':
-                # code...
-                $DeliveryManRate->star4 +=1;
-                break;
-            case '5':
-                # code...
-                $DeliveryManRate->star5 +=1;
-                break;
-            default:
-                # code...
-                break;
+        if($customer!=null)
+        {
+            $finishedOrder = $customer->orders()->where('status','=','2')->where('customer_id','=',$customer->id)->first();
+            $chooseMan = $finishedOrder->delivery_man_id;
+            $DeliveryManRate = DeliveryManRate::where('delivery_man_id','=',$chooseMan)->first();
+            $DeliveryManRate = DeliveryManRate::findorfail($DeliveryManRate->id);
+            $chooseRestaurant = $finishedOrder->restaurant_id;
+            $RestaurantRate = RestaurantRate::where('restaurant_id','=',$chooseRestaurant)->first();
+            $RestaurantRate = RestaurantRate::findorfail($RestaurantRate->id);        
+            $Rate = $DeliveryManRate;
+            $star = $request->delivery_man_star;
+            $AfterDeliveryManRate = $this->addStar($Rate,$star);$change_delivery = $AfterDeliveryManRate->getChanges();dd($change_delivery);
+            $Rate = $RestaurantRate;
+            $star = $request->restaurant_star;
+            $AfterRestaurantRate = $this->addStar($Rate,$star);
+            $customer['restaurantRate'] = $request->restaurant_star;
+            $customer['deliveryManRate'] = $request->delivery_man_star;
+            $customer['restaurant_id'] =$chooseRestaurant;
+            $customer['delivery_man_id'] =$chooseMan;
+            if($AfterDeliveryManRate->getChanges() != null)
+            {
+                $data = [
+                    "status" => 200,
+                    "method" => "rateOrder",
+                    "message" => "ok",
+                    "data" => $customer
+                ];
+                return response()->json($data, 200);
+            }else{
+                $data = [
+                    "status" => 404,
+                    "method" => "rateOrder",
+                    "message" => "錯誤",
+                    "data" => ""
+                ];
+            return response()->json($data, 404);
+            };
+        }else {
+            $data = [
+                "status" => 403,
+                "method" => "rateOrder",
+                "message" => "認證錯誤",
+                "data" => ""
+            ];
+            return response()->json($data, 403);
         }
-        $DeliveryManRate->save();
-        switch ($request->restaurant_star) {
-            case '1':
-                # code...
-                $restaurantRate->star1 +=1;
-                break;
-            case '2':
-                # code...
-                $restaurantRate->star2 +=1;
-                break;
-            case '3':
-                # code...
-                $restaurantRate->star3 +=1;
-                break;
-            case '4':
-                # code...
-                $restaurantRate->star4 +=1;
-                break;
-            case '5':
-                # code...
-                $restaurantRate->star5 +=1;
-                break;
-            default:
-                # code...
-                break;
-        }
-        $restaurantRate->save();
-        dd($restaurantRate);
-        
-        # code...
-
     }
 }
